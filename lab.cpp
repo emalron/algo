693532b1d3faa7ee02e3201eb8c9c12e83290ac2
Emalron <emalron.jes@gmail.com>
2019-08-28 15:29:48 +0900

    lab.cpp final

--- lab.cpp
+++ lab.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<vector>
 #include<queue>
+#include<map>
 
 struct Point {
     int x;
@@ -17,14 +18,14 @@ struct Stage {
     std::vector<Point> walls;
 };
 
-int floodfill(const Stage&, int**&, int**&, Point);
-int combi(const std::vector<Point>&, int, int, int*, std::vector<std::vector<Point>>&);
-int make_map(const Stage&, const std::vector<std::vector<Point>>&, std::vector<int**>&);
+int floodfill(const Stage&, int**&, Point, int&);
+int combi(const Stage&, int, int, int*, int**&);
+int dispute(const Stage&, int**&, std::vector<Point>);
+int max = 0;
 
 int main() {
     Stage stage;
-    Stage& stage_ref = stage;
-    int temp, zero_count, virus_count;
+    int temp;
     int** canvas;
 
     std::cin >> stage.row >> stage.column;
@@ -51,73 +52,24 @@ int main() {
                     break;
                 case 2:
                     stage.virus.push_back(Point {i, j});
+                    canvas[j][i] = 2;
                     break;
             }
         }
     }
 
     int flag[stage.size];
-    std::vector<std::vector<Point>> output;
-    std::vector<std::vector<Point>>& output_ref = output;
-    std::vector<Point> free_ref = stage.free;
-    combi(free_ref, free_ref.size(), 3, flag, output_ref);
+    combi(stage, stage.free.size(), 3, flag, canvas);
 
-    std::vector<int**> maps;
-    std::vector<int**>& maps_ref = maps;
-    maps.reserve(output.size());
+    std::cout << max << std::endl;
 
-    make_map(stage_ref, output_ref, maps_ref);
-    for(auto& map : maps) {
-        std::cout << "--MAP TEST---" << std::endl;
-        for(int j=0; j<stage.row; j++) {
-            for(int i=0; i<stage.column; i++) {
-                std::cout << map[j][i] << " ";
-            }
-            std::cout << std::endl;
-        }
+    for(int i=0;i<stage.row; i++) {
+        delete [] *(canvas+i);
     }
-
-    int**& canvas_ref = canvas;
-    virus_count = stage.virus.size();
-
-    std::cout << "---Dispute---" << std::endl;
-    for(auto& map : maps) {
-        int**& map_ref = map;
-        int zero = 0;
-        for(auto& virus : stage.virus) {
-            floodfill(stage_ref, map_ref, canvas_ref, virus);
-        }
-        for(int j=0; j<stage.row; j++) {
-            for(int i=0; i<stage.column; i++) {
-                if(map_ref[j][i] == 1) {
-                    std::cout << map_ref[j][i] << " ";
-                }
-                else {
-                    std::cout << canvas[j][i] << " ";
-                }
-                
-                if(canvas[j][i] != 2 && map_ref[j][i] == 0) {
-                    zero++;
-                }
-                canvas[j][i] = 0;
-            }
-            std::cout << std::endl;
-        } // DISPLAY: Result for each virus
-        std::cout << "free space: " << zero << std::endl;
-    }
-
-    std::cout << " - - - - " << std::endl;
-    for(int j=0; j<stage.row; j++) {
-        for(int i=0; i<stage.column; i++) {
-            std::cout << stage.map[j][i] << " ";
-        }
-        std::cout << std::endl;
-    } // DISPLAY: stage.map
-
-
+    delete [] canvas;
 }
 
-int floodfill(const Stage& stage, int**& map, int**& canvas, Point from) {
+int floodfill(const Stage& stage, int**& canvas, Point from, int& counter) {
     std::queue<Point> q;
 
     q.push(from);
@@ -127,8 +79,7 @@ int floodfill(const Stage& stage, int**& map, int**& canvas, Point from) {
     while(q.size() != 0) {
         Point current = q.front();
         q.pop();
-        canvas[current.y][current.x] = 2;
-
+        
         for(int i=0; i<4; i++) {
             int x_ = current.x + dx[i];
             int y_ = current.y + dy[i];
@@ -136,28 +87,31 @@ int floodfill(const Stage& stage, int**& map, int**& canvas, Point from) {
             bool inside_bound = (x_ < stage.column && x_ >= 0) && (y_ < stage.row && y_ >= 0);
             if(inside_bound) {
 
-                bool hit_wall = map[y_][x_] == 1;
+                bool hit_wall = stage.map[y_][x_] == 1;
                 bool is_visited = canvas[y_][x_] == 2;
                 if(!hit_wall && !is_visited) {
-                        q.push(Point{x_, y_});
+                    canvas[y_][x_] = 2;
+                    q.push(Point{x_, y_});
+                    counter++;
                 }
             }
         }
     }
 }
 
-int combi(const std::vector<Point>& free, int n, int r, int* flag, std::vector<std::vector<Point>>& output) {
-    std::vector<Point> temp;
+int combi(const Stage& stage, int n, int r, int* flag, int**& canvas) {
+    std::vector<Point> temp(3);
 
     if(r == 1) {
         for(int i=0; i<n; i++) {
             flag[i] = 1;
-            for(int j=0; j<free.size(); j++) {
+            for(int j=0; j<stage.free.size(); j++) {
                 if(flag[j] == 1) {
-                    temp.push_back(free[j]);
+                    temp.push_back(stage.free[j]);
                 }
             }
-            output.push_back(temp);
+
+            dispute(stage, canvas, temp);
             temp.clear();
             flag[i] = 0;
         }
@@ -165,41 +119,49 @@ int combi(const std::vector<Point>& free, int n, int r, int* flag, std::vector<s
     }
     if(r == n) {
         for(int i=0; i<n; i++) {
-            temp.push_back(free[i]);
+            temp.push_back(stage.free[i]);
         }
-        for(int j=0; j<free.size(); j++) {
+        for(int j=0; j<stage.free.size(); j++) {
             if(flag[j] == 1) {
-                temp.push_back(free[j]);
+                temp.push_back(stage.free[j]);
             }
         }
-        output.push_back(temp);
+
+        dispute(stage, canvas, temp);
         temp.clear();
         return 0;
     }
     else {
         flag[n-1] = 1;
-        combi(free, n-1, r-1, flag, output);
+        combi(stage, n-1, r-1, flag, canvas);
         flag[n-1] = 0;
-        combi(free, n-1, r, flag, output);
+        combi(stage, n-1, r, flag, canvas);
     }
 }
 
-int make_map(const Stage& stage, const std::vector<std::vector<Point>>& input, std::vector<int**>& maps) {
+int dispute(const Stage& stage, int**& canvas, std::vector<Point> map) {
+    
+    int zero, count = 0;
+
+    for(auto& wall : map) {
+        stage.map[wall.y][wall.x] = 1;
+    }
+
+    for(auto& virus : stage.virus) {
+        floodfill(stage, canvas, virus, count);
+    }
+
+    zero = stage.free.size() - 3 - count;
+
+    if(zero >= max) max = zero;
+
+    for(auto& wall : map) {
+        stage.map[wall.y][wall.x] = 0;
+    }
+
+    for(auto& free : stage.free) {
+        canvas[free.y][free.x] = 0;
+    }
 
     
-    for(auto& map : input) {
-        int** map_ = new int*[stage.row];
-        for(int i=0; i<stage.row; i++) {
-            map_[i] = new int[stage.column];
-        }
-
-        for(auto& wall : map) {
-            map_[wall.y][wall.x] = 1;
-        }
-        for(auto& wall : stage.walls) {
-            map_[wall.y][wall.x] = 1;
-        }
-
-        maps.push_back(map_);
-    }
 }
